fc6382c5ce435b309f0818c6afd9e7cc1c95d7a0
gastery0 <sebasgalindolon@gmail.com>
2026-08-10 08:19:46 -0500

    Update ejercicio01.pl

--- lab01/ejercicio01.pl
+++ lab01/ejercicio01.pl
@@ -1,1 +1,35 @@
+% Hechos: relación entre ciudades
+ciudad(bogota).
+ciudad(medellin).
+ciudad(cali).
+ciudad(cartagena).
+ciudad(manizales).
+ciudad(barranquilla).
+ciudad(pasto).
+ciudad(monteria).
 
+% Hechos: vuelos directos
+vuelo(bogota, medellin).
+vuelo(medellin, cartagena).
+vuelo(cali, bogota).
+vuelo(bogota, cartagena).
+vuelo(manizales, cartagena).
+vuelo(medellin, barranquilla).
+vuelo(pasto, bogota).
+vuelo(bogota, pasto).
+
+
+
+vuelo(bogota, medellin)
+true
+vuelo(bogota, DESTINO)
+DESTINO = medellin
+DESTINO = cartagena
+DESTINO = pasto
+vuelo(ORIGEN, medellin)
+ORIGEN = bogota
+vuelo(ORIGEN, bogota)
+ORIGEN = cali
+ORIGEN = pasto
+vuelo(ORIGEN, cali)
+false
